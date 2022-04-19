a578b1c7c240b386a96c75e10512bf0010784dd1
Saheb Dhara <sahebdhara961@gmail.com>
2022-04-19 21:28:17 +0530

    this is the first day of the last commit

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,8 +1,12 @@
 import Topbar from "../../components/topbar/topbar"
+import Feed from "../../components/feed/feed"
+import Topbar from "../../components/topbar/topbar"
+
 
 export default function home() {
   return ( 
   <Topbar/>
+  
   )
 }
 
