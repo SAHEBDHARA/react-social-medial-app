9a39f0be582b5959533624f21aee67d27fc23844
Saheb Dhara <sahebdhara961@gmail.com>
2022-04-22 22:58:35 +0530

    final commit

--- src/components/feed/feed.jsx
+++ src/components/feed/feed.jsx
@@ -1,9 +1,12 @@
+import Share from"../share/share"
 import "./feed.css"
 
 export default function Feed() {
   return (
     <div className="feed">
-        feed
+        <div className="feedWraper">
+        
+        </div>
         </div>
   )
 }
