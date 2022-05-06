f86d1a7c832e0cf2f28e2daed1e338c9e2d7bfbe
Saheb Dhara <sahebdhara961@gmail.com>
2022-05-06 20:53:57 +0530

    making rightbar

--- src/components/rightbar/rightbar.jsx
+++ src/components/rightbar/rightbar.jsx
@@ -10,6 +10,31 @@ export default function Rightbar() {
               <b>Sunetra Maity</b> and <b>3 other friends </b>have a birthday today
             </span>
           </div>
+          <img className="rightbarAd" src="accets\ad.png" alt=""/>
+          <h4 className="rightbarTitle">Online Friends</h4>
+          <ul className="rightBarFriendlist">
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img src="accets\person\5.jpeg" alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername"> Modon Lal</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img src="accets\person\2.jpeg" alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername"> Sourish Mondal</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img src="accets\person\4.jpeg" alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Sunetra Maity</span>
+            </li>
+          </ul>
         </div>
     </div>
   )
