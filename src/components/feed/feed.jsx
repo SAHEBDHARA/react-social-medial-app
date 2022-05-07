21be6133f002dd5adf9c2696f8d8dbbcef3d1d35
Saheb Dhara <sahebdhara961@gmail.com>
2022-05-07 11:22:45 +0530

    using dainamic data

--- src/components/feed/feed.jsx
+++ src/components/feed/feed.jsx
@@ -1,15 +1,16 @@
 import Share from"../share/share"
 import Post from "../post/post"
 import "./feed.css"
+import {Posts} from "../../dummyData"
 
 export default function Feed() {
   return (
     <div className="feed">
         <div className="feedWraper">
         <Share/>
-        <Post/>
-        <Post/>
-        <Post/>
+       {Posts.map((p)=>(
+        <Post key={p.id} post={p}/>
+        ))}
         </div>
         </div>
   )
--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -1,7 +1,8 @@
 import "./post.css"
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-export default function post() {
+export default function Post({post}) {
+  console.log(post);
   return (
     <div className="post">
         <div className="postWrapper">
@@ -9,24 +10,24 @@ export default function post() {
                 <div className="postLeft">
                   <img className="postProfileImg" src="\accets\person\4.jpeg" alt="" />
                   <span className="postUsername">Sunetra Maity</span>
-                  <span className="postDate">5 min ago</span>
+                  <span className="postDate">{post.date}</span>
                 </div>
                 <div className="postRight">
                   <MoreVertIcon/>
                 </div>
             </div>
             <div className="postCenter">
-              <span className="postTaxt"> Hey! Its my first post :)</span>
-              <img className="postImg" src="\accets\post\2.jpeg" alt="" />
+              <span className="postTaxt">{post?.desc}</span>
+              <img className="postImg" src={post.photo} alt="" />
             </div>
             <div className="postBotton">
               <div className="postBottonLeft">
                 <img className="LikeIcon" src="\accets\like.png" alt="" />
                 <img className="LikeIcon" src="\accets\heart.png" alt="" />
-                <span className="postLikeCounter">32 people like it</span>
+                <span className="postLikeCounter">{post.like}people like it</span>
               </div>
               <div className="postBottonRight">
-                <span className="postCommentText">8 comment</span>
+                <span className="postCommentText">{post.comment} comment</span>
               </div>
 
             </div>
