2269f641a0fee63fde842d4254c5d12a18f136c4
Saheb Dhara <sahebdhara961@gmail.com>
2022-05-12 22:08:00 +0530

    error is still showing

--- src/components/rightbar/rightbar.jsx
+++ src/components/rightbar/rightbar.jsx
@@ -1,4 +1,5 @@
 import "./rightbar.css"
+import {Users} from "../../dummyData"
 
 export default function Rightbar() {
   return (
@@ -13,27 +14,9 @@ export default function Rightbar() {
           <img className="rightbarAd" src="accets\ad.png" alt=""/>
           <h4 className="rightbarTitle">Online Friends</h4>
           <ul className="rightBarFriendlist">
-            <li className="rightbarFriend">
-              <div className="rightbarProfileImgContainer">
-                <img src="accets\person\5.jpeg" alt="" className="rightbarProfileImg" />
-                <span className="rightbarOnline"></span>
-              </div>
-              <span className="rightbarUsername"> Modon Lal</span>
-            </li>
-            <li className="rightbarFriend">
-              <div className="rightbarProfileImgContainer">
-                <img src="accets\person\2.jpeg" alt="" className="rightbarProfileImg" />
-                <span className="rightbarOnline"></span>
-              </div>
-              <span className="rightbarUsername"> Sourish Mondal</span>
-            </li>
-            <li className="rightbarFriend">
-              <div className="rightbarProfileImgContainer">
-                <img src="accets\person\4.jpeg" alt="" className="rightbarProfileImg" />
-                <span className="rightbarOnline"></span>
-              </div>
-              <span className="rightbarUsername">Sunetra Maity</span>
-            </li>
+           {Users.map((u) => (
+             <online key={u.id} user = {u} />
+           )) } 
           </ul>
         </div>
     </div>
