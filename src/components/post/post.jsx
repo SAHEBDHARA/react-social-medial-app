96402f3183bbe059ffc82be11e6ea821f35ff5d4
Saheb Dhara <sahebdhara961@gmail.com>
2022-07-20 22:12:36 +0530

    fd

--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -14,7 +14,7 @@ export default function Post({post}) {
         <div className="postWrapper">
             <div className="postTop">
                 <div className="postLeft">
-                  <img className="postProfileImg" src={Users.filter((u) => u.id === post.userId)[0].profilePicture} alt="" />
+                  <img className="postProfileImg" src={Users.filter((u) => u.id === post.userId)[0].profilePicture} alt="" /> // using filter 
                   <span className="postUsername">{Users.filter((u) => u.id === post.userId)[0].username}</span>
                   <span className="postDate">{post.date}</span>
                 </div>
