2401cb6073e054b059c741a23e6276b3249d0d2e
Saheb Dhara <sahebdhara961@gmail.com>
2022-08-03 21:28:18 +0530

    hti

--- src/components/feed/feed.jsx
+++ src/components/feed/feed.jsx
@@ -5,28 +5,28 @@ import { useEffect,useState } from "react";
 import axios from "axios";
 
 export default function Feed() {
-  const [posts,setPosts]=useState([]);
+//   const [posts,setPosts]=useState([]);
 
-  useEffect(()=>{
-    const fetchPost = async () =>{
-      const res = await axios.get("/posts/timeline/6257f0c1b2053f6f2d9a13a9")
-      console.log(res)
-    };
-    fetchPost()
+//   useEffect(()=>{
+//     const fetchPost = async () =>{
+//       const res = await axios.get("/posts/timeline/6257f0c1b2053f6f2d9a13a9")
+//       console.log(res)
+//     };
+//     fetchPost()
     
-  },[]);
+//   },[]);
 
   return (
     <div className="feed">
         <div className="feedWraper">
         <Share/>
-       {/* {Posts.map((p)=>(
+       {Posts.map((p)=>(
         <Post key={p.id} post={p}/>
-        ))} */}
+        ))}
         </div>
         </div>
   )
-}
+       }
 //hh
 
 
